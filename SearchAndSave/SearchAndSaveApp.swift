4e33c46e34880f239542b34281cbfce16059c2cd
Nakcheon Jung <ncjung@photowidget.net>
2025-05-15 14:42:43 +0900

    Initial Commit

---
+++ SearchAndSave/SearchAndSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAndSaveApp.swift
+//  SearchAndSave
+//
+//  Created by nakcheon.jung on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SearchAndSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
